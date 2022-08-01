8b34a73921c0dc83e87effc2f95d3bf275047243
shohjahon-sohibov <sohibov.s@bk.ru>
2022-08-01 13:01:32 +0500

    test

--- src/server.js
+++ src/server.js
@@ -12,9 +12,9 @@ connectDB()
 
 app.use(router);
 
-app.get('/', (_, res) => {
-  res.send('App works properly!');
-});
+// app.get('/', (_, res) => {
+//   res.send('App works properly!');
+// });
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`app works properly in PORT:${PORT}`))
